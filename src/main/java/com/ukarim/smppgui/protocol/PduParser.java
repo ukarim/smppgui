b89fefe8c07309581bb426d20947b930e5cb53b4
ukarim <mail@ukarim.com>
2022-10-08 17:49:07 +0600

    network stuff for smpp client was implemented

--- src/main/java/com/ukarim/smppgui/protocol/PduParser.java
+++ src/main/java/com/ukarim/smppgui/protocol/PduParser.java
@@ -1,8 +1,9 @@
 package com.ukarim.smppgui.protocol;
 
+import com.ukarim.smppgui.protocol.pdu.HeaderPdu;
 import com.ukarim.smppgui.protocol.pdu.Pdu;
 import java.nio.ByteBuffer;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 class PduParser {
@@ -10,6 +11,39 @@ class PduParser {
     private PduParser() {}
 
     public static List<Pdu> parsePdu(ByteBuffer buffer) throws SmppException {
-        return Collections.emptyList();
+        buffer.flip();
+        var pduList = new ArrayList<Pdu>();
+        Pdu pdu;
+        while ((pdu = parseSinglePdu(buffer)) != null) {
+            pduList.add(pdu);
+        }
+        return pduList;
+    }
+
+    private static Pdu parseSinglePdu(ByteBuffer buffer) throws SmppException {
+        if (!buffer.hasRemaining()) {
+            return null;
+        }
+        buffer.mark();
+        int cmdLen = buffer.getInt();
+        if (buffer.remaining() < (cmdLen - 4)) { // check if contains full pdu
+            buffer.reset();
+            return null;
+        }
+        var cmd = SmppCmd.fromCmdId(buffer.getInt());
+        var sts = SmppStatus.fromStatusId(buffer.getInt());
+        int seqNum = buffer.getInt();
+        switch (cmd) {
+            case UNBIND:
+            case UNBIND_RESP:
+            case GENERIC_NACK:
+            case CANCEL_SM_RESP:
+            case REPLACE_SM_RESP:
+            case ENQUIRE_LINK:
+            case ENQUIRE_LINK_RESP:
+                return new HeaderPdu(cmd, sts, seqNum);
+            default:
+                throw new SmppException("Unsupported smpp command: %s", cmd);
+        }
     }
 }
--- src/main/java/com/ukarim/smppgui/protocol/SmppClient.java
+++ src/main/java/com/ukarim/smppgui/protocol/SmppClient.java
@@ -1,18 +1,92 @@
 package com.ukarim.smppgui.protocol;
 
+import com.ukarim.smppgui.protocol.pdu.Pdu;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public final class SmppClient {
 
-    private final SmppHandler handler;
+    private static final int DEF_BUS_SIZE = 1024;
 
-    public SmppClient(SmppHandler handler) {
-        this.handler = handler;
+    private final AtomicBoolean active = new AtomicBoolean(true);
+
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(1);
+
+    private final SmppHandler smppHandler;
+
+    private SocketChannel channel;
+
+    public SmppClient(SmppHandler smppHandler) {
+        this.smppHandler = smppHandler;
     }
 
-    public void connect(String host, int port, String systemId, char[] password) {
-
+    public void sendPdu(Pdu pdu) throws IOException {
+        if (channel == null) {
+            throw new IllegalStateException("Smpp client not started");
+        }
+        var buffer = pdu.toByteBuffer();
+        buffer.flip(); // prepare for writing to socket
+        channel.write(buffer);
     }
 
-    public void disconnect() {
+    public void connect(String host, int port) throws IOException {
+        channel = SocketChannel.open(new InetSocketAddress(host, port));
+        active.set(true);
+        threadPool.execute(new SmppWorker());
+    }
 
+    public void disconnect() throws IOException {
+        // TODO send UNBIND pdu
+        shutdown();
+    }
+
+    private void shutdown() throws IOException {
+        active.set(false);
+        channel.close();
+        channel = null;
+    }
+
+    private class SmppWorker implements Runnable {
+
+        @Override
+        public void run() {
+            var buffer = ByteBuffer.allocate(DEF_BUS_SIZE);
+            buffer.order(ByteOrder.BIG_ENDIAN); // according to SMPP spec
+
+            while (active.get()) {
+                try {
+                    // TODO what if one pdu is larger than buffer's capacity?
+                    int readC = channel.read(buffer);
+                    if (readC == -1) {
+                        // channel closed
+                        smppHandler.handlePdu(null, new IOException("Connection was unexpectedly closed"));
+                        try {
+                            shutdown();
+                        } catch (IOException e) {}
+                        return;
+                    }
+                    if (readC == 0) {
+                        continue;
+                    }
+
+                    for (Pdu pdu : PduParser.parsePdu(buffer)) {
+                        Pdu respPdu = smppHandler.handlePdu(pdu, null);
+                        if (respPdu != null) {
+                            sendPdu(respPdu);
+                        }
+                    }
+
+                    buffer.compact(); // move unread bytes to the begging of array
+                } catch (Exception e) {
+                    smppHandler.handlePdu(null, e);
+                }
+            }
+        }
     }
 }
